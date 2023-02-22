b2180ca818f5f0f4ed44928ec03ad687148f702f
Arslan <arslankhanali@gmail.com>
2023-02-22 14:04:51 +1100

    wrapped flask model around best.pt

---
+++ model/backend.py
@@ -0,0 +1,35 @@
+"""
+Run a rest API exposing the yolov5s object detection model
+"""
+import argparse
+import io
+from PIL import Image
+
+import torch
+from flask import Flask, request
+
+app = Flask(__name__)
+
+DETECTION_URL = "/v1/object-detection/yolov5"
+
+
+@app.route(DETECTION_URL, methods=["POST"])
+def predict():
+    if not request.method == "POST":
+        return
+
+    if request.files.get("image"):
+        image_file = request.files["image"]
+        image_bytes = image_file.read()
+        img = Image.open(io.BytesIO(image_bytes))
+        results = model(img, size=640) # reduce size=320 for faster inference
+        return results.pandas().xyxy[0].to_json(orient="records")
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Flask api exposing yolov5 model")
+    parser.add_argument("--port", default=5000, type=int, help="port 5000")
+    parser.add_argument('--model', default='yolov5s', help='model to run, i.e. --model yolov5s')
+    args = parser.parse_args()
+
+    model = torch.hub.load('ultralytics/yolov5', args.model)
+    app.run(host="0.0.0.0", port=args.port)  # debug=True causes Restarting with stat
